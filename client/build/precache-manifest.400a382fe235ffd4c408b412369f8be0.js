2e36925118824718a524b66e020f51697a58cd09
Julian Bull <julianbullmagic@gmail.com>
2023-05-31 21:50:16 +1000

    ffff

---
+++ client/build/precache-manifest.400a382fe235ffd4c408b412369f8be0.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "7b605ede3d617e680813",
+    "url": "/static/css/main.77d8fb0b.chunk.css"
+  },
+  {
+    "revision": "7b605ede3d617e680813",
+    "url": "/static/js/main.681414fc.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "7e461d19cd9d95312ffb",
+    "url": "/static/css/2.eee6f457.chunk.css"
+  },
+  {
+    "revision": "7e461d19cd9d95312ffb",
+    "url": "/static/js/2.aacea293.chunk.js"
+  },
+  {
+    "revision": "0cd2a94ff1dfde15c9890d6460340651",
+    "url": "/static/media/hands.0cd2a94f.png"
+  },
+  {
+    "revision": "96a4179fee64f893e8e09271cd0f2c12",
+    "url": "/static/media/housing-prices-Australia.96a4179f.png"
+  },
+  {
+    "revision": "65f9eafa1947f2145e53459074978fcb",
+    "url": "/static/media/WageSlowdown.65f9eafa.jpg"
+  },
+  {
+    "revision": "d5a8701e59d8516da228d0df31393dcb",
+    "url": "/static/media/Australian-unemployment-rates.d5a8701e.png"
+  },
+  {
+    "revision": "e2c0dc8cbec5fc726d0158134e688953",
+    "url": "/static/media/Trade-union-density-in-Australia-percentage-of-employees-1960-2011-a-a-Trade-union.e2c0dc8c.png"
+  },
+  {
+    "revision": "2b8002b74ddf6761efc92024c7e97de7",
+    "url": "/static/media/robots-txt.2b8002b7.config"
+  },
+  {
+    "revision": "3e56340a3caca61a1e3c640d5ef7cc84",
+    "url": "/index.html"
+  }
+];
